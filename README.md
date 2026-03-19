ad796f5d88d9f379ca11284f5648aaf96857acaf
Shaheer <eng.sh.muhammed@gmail.com>
2026-03-19 02:45:30 +0200

    Add screenshots and improve UI documentation

--- README.md
+++ README.md
@@ -91,7 +91,7 @@ Step 5: Open the browser
 
 http://127.0.0.1:5000
 
-## Application Screenshots
+## Application Demo
 
 ### Home Page
 
--- crypto/rsa_utils.py
+++ crypto/rsa_utils.py
@@ -3,7 +3,6 @@ from cryptography.hazmat.primitives import serialization, hashes
 
 
 def generate_keys():
-
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048
@@ -15,17 +14,20 @@ def generate_keys():
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.NoEncryption()
-    )
+    ).decode()
 
     public_pem = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+    ).decode()
 
-    return public_pem.decode(), private_pem.decode()
+    return public_pem, private_pem
 
 
 def encrypt_message(public_key_pem, message):
+
+    if public_key_pem is None:
+        raise Exception("Receiver public key not found")
 
     public_key = serialization.load_pem_public_key(
         public_key_pem.encode()
@@ -40,10 +42,10 @@ def encrypt_message(public_key_pem, message):
         )
     )
 
-    return encrypted.hex()
+    return encrypted
 
 
-def decrypt_message(private_key_pem, ciphertext):
+def decrypt_message(private_key_pem, encrypted_message):
 
     private_key = serialization.load_pem_private_key(
         private_key_pem.encode(),
@@ -51,7 +53,7 @@ def decrypt_message(private_key_pem, ciphertext):
     )
 
     decrypted = private_key.decrypt(
-        bytes.fromhex(ciphertext),
+        encrypted_message,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
